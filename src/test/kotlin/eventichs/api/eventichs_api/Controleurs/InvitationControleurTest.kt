71fa46f6193f2d75852fe830466cda76ebf12503
julienG <jgiguere@dept-info.crosemont.qc.ca>
2023-11-05 22:08:06 -0500

    Création ficher test InvitationControleurTest.kt

---
+++ src/test/kotlin/eventichs/api/eventichs_api/Controleurs/InvitationControleurTest.kt
@@ -0,0 +1,30 @@
+package eventichs.api.eventichs_api.Controleurs
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.web.servlet.MockMvc
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class InvitationControleurTest {
+
+//    @MockBean
+//    lateinit var service: InvitationService
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    //Julien
+    @Test
+    fun `Étant donné lorsque alors` (){
+        TODO("Méthode non-implémentée")
+    }
+
+    //Alex
+
+
+
+}
