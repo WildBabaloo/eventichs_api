6070cddd5b22bcd1709407cef120fcd6b1ad7818
agascon <agasc@mon-ordi>
2023-11-05 23:54:08 -0500

    Écriture de plusieurs tests pour le controlleur d'invitation

--- src/test/kotlin/eventichs/api/eventichs_api/Controleurs/InvitationControleurTest.kt
+++ src/test/kotlin/eventichs/api/eventichs_api/Controleurs/InvitationControleurTest.kt
@@ -25,6 +25,51 @@ class InvitationControleurTest {
 
     //Alex
 
+    @Test
+    fun `Étant donné l'utilisateur dont l'id est 1, qui ne fait pas partie d'aucune organisation, lorsqu'on effectue une requete POST pour ajouter une demande d'invitation à l'organisation éxistante dont l'id est 2, on obtient un JSON qui contient un évènement qui contient l'id utilisateur, l'id organisation et l'état de la demande, ainsi d'un code de retour 201` (){
+        TODO("Méthode non-implémentée")
+    }
+
+    @Test
+    fun `Étant donné l'utilisateur dont l'id est 1 qui fait déja partie de l'organisation dont l'id est 2, lorsqu'on effectue une requete POST pour ajouter une demande d'invitation à l'organisation éxistante dont l'id est 2, on obtient le code de retour 409` (){
+        TODO("Méthode non-implémentée")
+    }
+
+    @Test
+    fun `Étant donné l'utilisateur dont l'id est 1 lorsqu'on effectue une requete POST pour ajouter une demande d'invitation à une organisation non-éxistente dont l'id est -1, on obtient le code de retour 400` (){
+        TODO("Méthode non-implémentée")
+    }
+
+    @Test
+    fun `Étant donné l'utilisateur dont l'id est 1, qui est inscrit à l'évènement dont l'id est 100 et le type = publique, lorsqu'on effectue une requete POST pour envoyer une invitation à l'utilisateur 2 qui n'est pas inscrit à l'évènement avec l'id 100, on obtient un JSON qui contient l'invitation avec un id utilisateur-invitateur, un id utilisateur-invité, un id évènement, l'état de l'invitation ainsi qu'un code de retour 201` (){
+        TODO("Méthode non-implémentée")
+    }
+
+    @Test
+    fun `Étant donné l'utilisateur dont l'id est 1, qui n'est pas inscrit à l'évènement dont l'id est 100 et le type = publique, lorsqu'on effectue une requete POST pour envoyer une invitation à l'utilisateur 2 qui n'est pas inscrit à l'évènement avec l'id 100, on obtient un code de retour 400` (){
+        TODO("Méthode non-implémentée")
+    }
+
+    @Test
+    fun `Étant donné l'utilisateur dont l'id est 1, est inscrit à l'évènement dont l'id est 100 et le type = publique, lorsqu'on effectue une requete POST pour envoyer une invitation à l'utilisateur 2 qui est déja inscrit à l'évènement avec l'id 100, on obtient un code de retour 409` (){
+        TODO("Méthode non-implémentée")
+    }
+
+    @Test
+    fun `Étant donné l'utilisateur dont l'id est 1, est inscrit à l'évènement dont l'id est 100 et le type = privé, lorsqu'on effectue une requete POST pour envoyer une invitation à l'utilisateur 2 n'est pas déja inscrit à l'évènement avec l'id 100, on obtient un code de retour 409` (){
+        TODO("Méthode non-implémentée")
+    }
+
+    @Test
+    fun `Étant donné l'utilisateur dont l'id est 1, lorsqu'on effectue une requete GET de recherche d'invitations par id utilisateur, on obtient un fichier JSON qui contient la liste des invitations dont l'id de l'invité est 1 et un code de retour 200`(){
+        TODO("Méthode non-implémentée")
+    }
+
+    @Test
+    fun `Étant donné l'utilisateur dont l'id est 1 et qui n'est pas inscrit au service, lorsqu'on effectue une requete GET de recherche d'invitations par id utilisateur, on obtient le code de retour 404`(){
+        TODO("Méthode non-implémentée")
+    }
+
 
 
 }
