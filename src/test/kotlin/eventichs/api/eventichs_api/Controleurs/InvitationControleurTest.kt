de548e22a49f2e86aef90a87ea61c12f22a5ee17
agascon <agasc@mon-ordi>
2023-11-06 16:19:13 -0500

    Tests unitaires pour Invitation complétés

--- src/test/kotlin/eventichs/api/eventichs_api/Controleurs/InvitationControleurTest.kt
+++ src/test/kotlin/eventichs/api/eventichs_api/Controleurs/InvitationControleurTest.kt
@@ -33,6 +33,8 @@ class InvitationControleurTest {
 
     //Alex
 
+
+    //Demander à joindre une organisation
     @Test
     fun `Étant donné l'utilisateur dont l'id est 1, qui ne fait pas partie d'aucune organisation, lorsqu'on effectue une requete POST pour ajouter une demande d'invitation à l'organisation éxistante dont l'id est 2, on obtient un JSON qui contient un évènement qui contient l'id utilisateur, l'id organisation et l'état de la demande, ainsi d'un code de retour 201` (){
         TODO("Méthode non-implémentée")
@@ -48,6 +50,8 @@ class InvitationControleurTest {
         TODO("Méthode non-implémentée")
     }
 
+
+    //Inviter un autre participant à un événement publique (Participant)
     @Test
     fun `Étant donné l'utilisateur dont l'id est 1, qui est inscrit à l'évènement dont l'id est 100 et le type = publique, lorsqu'on effectue une requete POST pour envoyer une invitation à l'utilisateur 2 qui n'est pas inscrit à l'évènement avec l'id 100, on obtient un JSON qui contient l'invitation avec un id utilisateur-invitateur, un id utilisateur-invité, un id évènement, l'état de l'invitation ainsi qu'un code de retour 201` (){
         TODO("Méthode non-implémentée")
@@ -68,8 +72,9 @@ class InvitationControleurTest {
         TODO("Méthode non-implémentée")
     }
 
+    // Consulter ses invitations et ses demandes (Participant + Organisation)
     @Test
-    fun `Étant donné l'utilisateur dont l'id est 1, lorsqu'on effectue une requete GET de recherche d'invitations par id utilisateur, on obtient un fichier JSON qui contient la liste des invitations dont l'id de l'invité est 1 et un code de retour 200`(){
+    fun `Étant donné l'utilisateur dont l'id est 1, lorsqu'on effectue une requete GET de recherche d'invitations et demandes par id utilisateur, on obtient un fichier JSON qui contient la liste des invitations dont l'id de l'invité est 1 et un code de retour 200`(){
         TODO("Méthode non-implémentée")
     }
 
@@ -78,6 +83,15 @@ class InvitationControleurTest {
         TODO("Méthode non-implémentée")
     }
 
+    //Accepter la demande de joindre l'organisation par le participant
 
+    @Test
+    fun `Etant donne l'organisation dont l'id est 1, lorsqu'on effectue une requête POST pour accepter la demande d'invitation de l'utilisateur dont l'id est 2, on obtient un JSON qui contient l'id de l'utilisateur, l'id de l'organisation et un code de retour 201 `(){
+        TODO("Methode non-implementee")
+    }
 
+    @Test
+    fun `Étant donné l'organisation dont l'id est 1, lorsqu'on effectue une requête POST pour accepter une demande non éxistante de l'utilisateur dont l'id est 2, on obtient un code de retour 409 `(){
+        TODO("Methode non-implementee")
+    }
 }
