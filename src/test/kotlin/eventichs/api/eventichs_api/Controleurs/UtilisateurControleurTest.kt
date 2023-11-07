94ce7062976ee24543c6283c28a57062f23c69a4
WildBabaloo <47115136+WildBabaloo@users.noreply.github.com>
2023-11-06 23:40:58 -0500

    Modification des fichiers pour le mettre tous dans organisation

--- src/test/kotlin/eventichs/api/eventichs_api/Controleurs/UtilisateurControleurTest.kt
+++ src/test/kotlin/eventichs/api/eventichs_api/Controleurs/UtilisateurControleurTest.kt
@@ -5,43 +5,4 @@ class UtilisateurControleurTest {
     @Autowired
     private lateinit var mockMvc: MockMvc
 
-    @Test
-    fun `Étant donné un participant qui effectue une requete GET pour consulter la liste des organisations publiques on obteint un JSON qui contient la liste des organisation publiques et un code de retour 200`(){
-        TODO("Méthode non-implémentée")
-    }
-
-    @Test
-    fun `Étant donné un participant qui effectue une requete GET pour consulter la liste des organisations publiques on obteint un code de retour 404`(){
-        TODO("Méthode non-implémentée")
-    }
-
-    @Test
-    fun `Étant donné un participant qui effectue une requete GET pour filtrer la liste des organisations publiques selon leurs goûts on obtient un code de retour 404`(){
-        TODO("Méthode non-implémentée")
-    }
-
-    @Test
-    fun `Étant donné un participant qui effectue une requete GET pour filtrer la liste des organisations publiques selon leur goûts on obteint un JSON qui contient la liste filtrée des organisation publiques et un code de retour 200`(){
-        TODO("Méthode non-implémentée")
-    }
-
-    @Test
-    fun `Étant donné un participant qui effectue une requete GET pour consulter la liste organisations rejointes on obtient un code de retour 404`(){
-        TODO("Méthode non-implémentée")
-    }
-
-    @Test
-    fun `Étant donné un participant qui effectue une requete GET pour consulter la liste des organisations rejointes on obteint un JSON qui contient la liste des organisation rejointes et un code de retour 200`(){
-        TODO("Méthode non-implémentée")
-    }
-
-    @Test
-    fun `Étant donné un participant qui effectue une requete POST pour quitter une organisation rejointe qui n'existe pas, on obtient un code de retour 404`(){
-        TODO("Méthode non-implémentée")
-    }
-
-    @Test
-    fun `Étant donné un participant qui effectue une requete POST pour quitter une organisation on obteint un code de retour 200`(){
-        TODO("Méthode non-implémentée")
-    }
 }
