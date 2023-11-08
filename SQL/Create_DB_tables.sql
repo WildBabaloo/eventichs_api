3ef710c9c79fd22e543427933de426874d0bc268
mfatene <2023073@crosemont.qc.ca>
2023-11-08 17:13:30 -0500

    BASE DE DONNÉES version 1 sans jointures

--- SQL/Create_DB_tables.sql
+++ SQL/Create_DB_tables.sql
@@ -5,10 +5,28 @@ use `eventichsBD`;
 -- ----------------------------------------------------------------------------------------------
 -- TABLE UTILISATEUR
 
+CREATE TABLE utilisateur (
+    code VARCHAR(255) NOT NULL,
+    nom VARCHAR(255) NOT NULL,
+    courriel VARCHAR(255) NOT NULL UNIQUE,
+    motDePasse VARCHAR(255) NOT NULL
+);
+
 -- TABLE INVITATION
-
+CREATE TABLE Invitation (
+    code VARCHAR(255) NOT NULL,
+    codeExpediteur VARCHAR(255) NOT NULL,
+    codeDestinataire VARCHAR(255) NOT NULL,
+    codeQuoiRejoindre_id VARCHAR(255) NOT NULL,
+    status SET('envoyé', 'accepté', 'refusé')
+);
 -- TABLE JETON
-
+CREATE TABLE Jeton (
+   id INT AUTO_INCREMENT,
+   codeQuoiRejoindre VARCHAR(255) NOT NULL,
+   jeton VARCHAR(255) NOT NULL,
+   PRIMARY KEY (id)
+);
 
 
 
@@ -16,11 +34,31 @@ use `eventichsBD`;
 
 -- -----------------------------------------------------------------------------------------------
 -- TABLE EVENEMENT
+CREATE TABLE Événement (
+   code VARCHAR(255) NOT NULL,
+   nom VARCHAR(255) NOT NULL,
+   dateDebut DATE NOT NULL,
+   dateFin DATE NOT NULL,
+   type VARCHAR(255) NOT NULL,
+   categorie_id VARCHAR(255) NOT NULL,
+   description VARCHAR(255),
+   photo VARCHAR(255),
+   organisation_id VARCHAR(255) NOT NULL
+);
 
 -- TABLE CATÉGORIE
+CREATE TABLE Catégorie (
+   id INT AUTO_INCREMENT,
+   nom VARCHAR(255) NOT NULL,
+   description VARCHAR(255),
+   PRIMARY KEY (id)
+);
 
 -- TABLE MEMBRE_ÉVÉNEMENT
-
+CREATE TABLE Membres_événement (
+   idUtilisateur VARCHAR(255) NOT NULL,
+   idEvenement VARCHAR(255) NOT NULL
+);
 
 
 
@@ -29,6 +67,19 @@ use `eventichsBD`;
 
 -- -----------------------------------------------------------------------------------------------
 -- TABLE ORGANISATION
+CREATE TABLE Organisation (
+  code INT NOT NULL,
+  codeUtilisateur INT NOT NULL,
+  nom VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  motDePasse VARCHAR(255) NOT NULL,
+  catégorie_id INT NOT NULL,
+  estPublic BOOL NOT NULL
+);
 
 -- TABLE ORGANISATION_MEMBRE
-
+CREATE TABLE Organisations_membres (
+    code VARCHAR(255) NOT NULL,
+    code_organisation INT NOT NULL,
+    code_utilisateur INT NOT NULL
+);
