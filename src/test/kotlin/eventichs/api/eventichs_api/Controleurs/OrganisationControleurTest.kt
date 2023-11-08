3c1685d86759acd051bee287dd9c1ede37c9039c
mfatene <2023073@crosemont.qc.ca>
2023-11-08 16:01:08 -0500

    Correction de la classe test organisation

--- src/test/kotlin/eventichs/api/eventichs_api/Controleurs/OrganisationControleurTest.kt
+++ src/test/kotlin/eventichs/api/eventichs_api/Controleurs/OrganisationControleurTest.kt
@@ -1,5 +1,12 @@
 package test.kotlin.eventichs.api.eventichs_api.Controleurs
 
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+@SpringBootTest
+@AutoConfigureMockMvc
 class OrganisationControleurTest {
 
 
