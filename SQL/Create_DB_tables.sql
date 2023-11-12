79ad57d2a9e1eb9e33ac750993c9a332868ed8c5
julienG <jgiguere@dept-info.crosemont.qc.ca>
2023-11-12 15:12:09 -0500

    Modification des tables Invitation et Jeton ainsi qu'un script INSERT de test

--- SQL/Create_DB_tables.sql
+++ SQL/Create_DB_tables.sql
@@ -6,46 +6,15 @@ use `eventichsBD`;
 -- TABLE UTILISATEUR
 
 CREATE TABLE utilisateur (
-    code VARCHAR(255) NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(255) NOT NULL,
     courriel VARCHAR(255) NOT NULL UNIQUE,
     motDePasse VARCHAR(255) NOT NULL
 );
 
--- TABLE INVITATION
-CREATE TABLE Invitation (
-    code VARCHAR(255) NOT NULL,
-    codeExpediteur VARCHAR(255) NOT NULL,
-    codeDestinataire VARCHAR(255) NOT NULL,
-    codeQuoiRejoindre_id VARCHAR(255) NOT NULL,
-    status SET('envoyé', 'accepté', 'refusé')
-);
--- TABLE JETON
-CREATE TABLE Jeton (
-   id INT AUTO_INCREMENT,
-   codeQuoiRejoindre VARCHAR(255) NOT NULL,
-   jeton VARCHAR(255) NOT NULL,
-   PRIMARY KEY (id)
-);
-
 
 
 -- -----------------------------------------------------------------------------------------------
--- TABLE EVENEMENT
-CREATE TABLE Événement (
-   code VARCHAR(255) NOT NULL,
-   nom VARCHAR(255) NOT NULL,
-   dateDebut DATE NOT NULL,
-   dateFin DATE NOT NULL,
-   type VARCHAR(255) NOT NULL,
-   categorie_id VARCHAR(255) NOT NULL,
-   description VARCHAR(255),
-   photo VARCHAR(255),
-   organisation_id VARCHAR(255) NOT NULL,
-   PRIMARY KEY (code),
-   FOREIGN KEY (categorie_id) REFERENCES Catégorie(id)
-);
-
 -- TABLE CATÉGORIE
 CREATE TABLE Catégorie (
    id INT AUTO_INCREMENT,
@@ -54,13 +23,30 @@ CREATE TABLE Catégorie (
    PRIMARY KEY (id)
 );
 
+-- TABLE EVENEMENT
+CREATE TABLE Événement (
+   id int NOT NULL AUTO_INCREMENT,
+   nom VARCHAR(255) NOT NULL,
+   dateDebut DATE NOT NULL,
+   dateFin DATE NOT NULL,
+   type VARCHAR(255) NOT NULL,
+   categorie_id int NOT NULL,
+   description VARCHAR(255),
+   photo VARCHAR(255),
+   organisation_id VARCHAR(255) NOT NULL,
+   PRIMARY KEY (id),
+   FOREIGN KEY (categorie_id) REFERENCES Catégorie(id)
+);
+
+
+
 -- TABLE MEMBRE_ÉVÉNEMENT
 CREATE TABLE Membres_événement (
-   idUtilisateur VARCHAR(255) NOT NULL,
-   idEvenement VARCHAR(255) NOT NULL,
+   idUtilisateur int NOT NULL,
+   idEvenement int NOT NULL,
    PRIMARY KEY (idUtilisateur, idEvenement),
-   FOREIGN KEY (idUtilisateur) REFERENCES utilisateur(code)
-   FOREIGN KEY (idEvenement) REFERENCES Événement(code)
+   FOREIGN KEY (idUtilisateur) REFERENCES utilisateur(id),
+   FOREIGN KEY (idEvenement) REFERENCES Événement(id)
 );
 
 
@@ -68,21 +54,49 @@ CREATE TABLE Membres_événement (
 -- -----------------------------------------------------------------------------------------------
 -- TABLE ORGANISATION
 CREATE TABLE Organisation (
-  code INT NOT NULL,
-  codeUtilisateur INT NOT NULL,
+  id int NOT NULL AUTO_INCREMENT,
+  idUtilisateur INT NOT NULL,
   catégorie_id INT NOT NULL,
-  estPublic BOOL NOT NULL,
-  PRIMARY KEY (code),
+  estPublic BOOL NOT NULL DEFAULT FALSE,
+  PRIMARY KEY (id),
   FOREIGN KEY (catégorie_id) REFERENCES Catégorie(id),
-  FOREIGN KEY (codeUtilisateur) REFERENCES utilisateur(code)
+  FOREIGN KEY (idUtilisateur) REFERENCES utilisateur(id)
 );
 
 -- TABLE ORGANISATION_MEMBRE
 CREATE TABLE Organisations_membres (
-    code VARCHAR(255) NOT NULL,
-    code_organisation INT NOT NULL,
-    code_utilisateur INT NOT NULL,
-    PRIMARY KEY (code),
-    FOREIGN KEY (code_organisation) REFERENCES Organisation(code),
-    FOREIGN KEY (code_utilisateur) REFERENCES utilisateur(code)
+    id_organisation int NOT NULL,
+    id_utilisateur INT NOT NULL,
+    PRIMARY KEY (id_organisation,id_utilisateur),
+    FOREIGN KEY (id_organisation) REFERENCES Organisation(id),
+    FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id)
+);
+
+
+-- -----------------------------------------------------------------------------------------------
+-- TABLE INVITATION
+CREATE TABLE Invitation (
+    idExpediteur int NOT NULL, -- Qui veux deamander/Inviter à joindre?
+    idDestinataire int NOT NULL, -- Le réceptionnaire de la demande/invitation. Celui qui doit l'accepter ou la refuser.
+    idQuoiRejoindre int NOT NULL, -- Ce que l'expediteur cherche à joindre
+    model SET('Organisation', 'Événement'), -- L'expediteur cherche à joindre une organisation ou un événement?
+	status SET('envoyé', 'accepté', 'refusé'), -- L'état de la demande/invitation.
+	PRIMARY KEY (idExpediteur, idDestinataire, idQuoiRejoindre, model),
+	organisation_id INT AS (IF(model = 'Organisation', idQuoiRejoindre, NULL)) STORED,  -- Les 4 lignes suivantes pertmettent de guarantir l'intégrité des données.
+    événement_id INT AS (IF(model = 'Événement', idQuoiRejoindre, NULL)) STORED,        -- Les colonnes organisation_id et événement_id sont remplis automatiquement et ne doive pas être rempli dans un INSERT.
+    FOREIGN KEY (`organisation_id`) REFERENCES Organisation(id) ON DELETE CASCADE,
+    FOREIGN KEY (`événement_id`) REFERENCES Événement(id) ON DELETE CASCADE
+);
+
+-- TABLE JETON
+CREATE TABLE Jeton (
+   id INT AUTO_INCREMENT,
+   idQuoiRejoindre int NOT NULL, -- À quel organisation ou événement le jeton réfère.
+   model SET('Organisation', 'Événement'), -- Est-ce une organisation ou un événement.
+   jeton VARCHAR(255) NOT NULL,
+   PRIMARY KEY (id),
+   organisation_id INT AS (IF(model = 'Organisation', idQuoiRejoindre, NULL)) STORED,  -- Les 4 lignes suivantes pertmettent de guarantir l'intégrité des données.
+   événement_id INT AS (IF(model = 'Événement', idQuoiRejoindre, NULL)) STORED,        -- Les colonnes organisation_id et événement_id sont remplis automatiquement et ne doive pas être rempli dans un INSERT.
+   FOREIGN KEY (`organisation_id`) REFERENCES Organisation(id) ON DELETE CASCADE,
+   FOREIGN KEY (`événement_id`) REFERENCES Événement(id) ON DELETE CASCADE
 );
---
+++ SQL/insert_DB_tables.sql
@@ -0,0 +1,24 @@
+-- Insertion Utilisateur
+INSERT INTO `eventichsbd`.`utilisateur` (`nom`, `courriel`, `motDePasse`) VALUES ('joe', 'joegmail', '123');
+INSERT INTO `eventichsbd`.`utilisateur` (`nom`, `courriel`, `motDePasse`) VALUES ('sam', 'samgmail', '123');
+
+-- Insertion Catégorie
+INSERT INTO `eventichsbd`.`catégorie` (`nom`, `description`) VALUES ('allo', 'allo');
+
+-- Insertion Organisation
+INSERT INTO `eventichsbd`.`organisation` (`idUtilisateur`, `catégorie_id`) VALUES ('1', '1');
+
+-- Insertion Organisations_membres
+
+-- Insertion Événement
+INSERT INTO `eventichsbd`.`événement` (`nom`, `dateDebut`, `dateFin`, `type`, `categorie_id`, `description`, `photo`, `organisation_id`) VALUES ('Soiré', '2020-10-10', '2020-10-10', 'soiré', '1', 'aaa', 'aaa', '1');
+INSERT INTO `eventichsbd`.`événement` (`nom`, `dateDebut`, `dateFin`, `type`, `categorie_id`, `description`, `photo`, `organisation_id`) VALUES ('Soiré2', '2020-10-10', '2020-10-10', 'soiré', '1', 'aaa', 'aaa', '1');
+INSERT INTO `eventichsbd`.`événement` (`nom`, `dateDebut`, `dateFin`, `type`, `categorie_id`, `description`, `photo`, `organisation_id`) VALUES ('Soiré3', '2020-10-10', '2020-10-10', 'soiré', '1', 'aaa', 'aaa', '1');
+
+-- Insertion Membres_événement
+
+-- Insertion Invitation
+INSERT INTO `eventichsbd`.`invitation` (`idExpediteur`, `idDestinataire`, `idQuoiRejoindre`, `model`, `status`) VALUES ('1', '2', '1', 'Organisation', 'envoyé');
+INSERT INTO `eventichsbd`.`invitation` (`idExpediteur`, `idDestinataire`, `idQuoiRejoindre`, `model`, `status`) VALUES ('1', '2', '3', 'Événement', 'envoyé');
+
+-- Insertion Jeton
