60904872f41ceb0eae8ae3cd9b1ef00ad759009f
WildBabaloo <47115136+WildBabaloo@users.noreply.github.com>
2023-11-18 00:40:30 -0500

    Init branche api_gestion_organisation

--- src/main/kotlin/eventichs/api/eventichs_api/DAO/OrganisationDAOImplMémoire.kt
+++ src/main/kotlin/eventichs/api/eventichs_api/DAO/OrganisationDAOImplMémoire.kt
@@ -3,4 +3,5 @@ package eventichs.api.eventichs_api.DAO
 import org.springframework.jdbc.core.JdbcTemplate
 
 class OrganisationDAOImplMémoire(val db: JdbcTemplate): OrganisationDAO {
+    // TODO
 }
