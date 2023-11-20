70e3115c42506a8ee083fe9f6a7caba713a7d8af
WildBabaloo <47115136+WildBabaloo@users.noreply.github.com>
2023-11-19 23:14:56 -0500

    Changé les noms des variables

--- src/main/kotlin/eventichs/api/eventichs_api/DAO/OrganisationDAO.kt
+++ src/main/kotlin/eventichs/api/eventichs_api/DAO/OrganisationDAO.kt
@@ -2,14 +2,13 @@ package eventichs.api.eventichs_api.DAO
 
 import eventichs.api.eventichs_api.Modèle.Categorie
 import eventichs.api.eventichs_api.Modèle.Organisation
-import eventichs.api.eventichs_api.Modèle.Utilisateur
 
 interface OrganisationDAO: DAO<Organisation> {
     override fun chercherTous(): List<Organisation>
-    override fun chercherParID(codeOrganisation: Int): Organisation?
-    override fun ajouter(uneOrganisation: Organisation): Organisation?
-    override fun supprimerParID(codeOrganisation: Int): Organisation?
-    override fun modifier(uneOrganisation: Organisation): Organisation?
+    override fun chercherParID(id: Int): Organisation?
+    override fun ajouter(element: Organisation): Organisation?
+    override fun supprimerParID(id: Int): Organisation?
+    override fun modifier(element: Organisation): Organisation?
     fun consulterOrganisationPubliques(): List<Organisation>
     fun filtrerOrganisationParGouts(uneCategorie: Categorie): List<Organisation>
     fun changerVisiblitéOrganisation(codeOrganisation: Organisation, estPublic: Boolean): Organisation?
--- src/main/kotlin/eventichs/api/eventichs_api/DAO/OrganisationDAOImplMémoire.kt
+++ src/main/kotlin/eventichs/api/eventichs_api/DAO/OrganisationDAOImplMémoire.kt
@@ -2,9 +2,7 @@ package eventichs.api.eventichs_api.DAO
 
 import eventichs.api.eventichs_api.Modèle.Categorie
 import eventichs.api.eventichs_api.Modèle.Organisation
-import eventichs.api.eventichs_api.Modèle.Utilisateur
 import org.springframework.jdbc.core.JdbcTemplate
-import org.springframework.jdbc.core.queryForObject
 import org.springframework.stereotype.Repository
 
 @Repository
@@ -12,34 +10,34 @@ class OrganisationDAOImplMémoire(val db: JdbcTemplate): OrganisationDAO {
     override fun chercherTous(): List<Organisation> =
         db.query("select * from organisation", OrganisationMapper())
 
-    override fun chercherParID(codeOrganisation: Int): Organisation? =
-        db.queryForObject("select * from organisation where id = $codeOrganisation", OrganisationMapper())
+    override fun chercherParID(id: Int): Organisation? =
+        db.queryForObject("select * from organisation where id = $id", OrganisationMapper())
 
-    override fun ajouter(uneOrganisation: Organisation): Organisation? {
+    override fun ajouter(element: Organisation): Organisation? {
         db.update(
             "insert into organisation values (?, ?, ?, ?)",
-            uneOrganisation.id,
-            uneOrganisation.idUtilisateur,
-            uneOrganisation.nomOrganisation,
-            uneOrganisation.catégorie_id,
-            uneOrganisation.estPublic
+            element.id,
+            element.idUtilisateur,
+            element.nomOrganisation,
+            element.catégorie_id,
+            element.estPublic
         )
-        return uneOrganisation
+        return element
     }
 
-    override fun modifier(uneOrganisation: Organisation): Organisation? {
+    override fun modifier(element: Organisation): Organisation? {
         db.update(
-            "update organisation set nomOrganisation = ?, catégorie_id = ? where id = $uneOrganisation.id",
-            uneOrganisation.nomOrganisation,
-            uneOrganisation.catégorie_id
+            "update organisation set nomOrganisation = ?, catégorie_id = ? where id = $element.id",
+            element.nomOrganisation,
+            element.catégorie_id
         )
-        return uneOrganisation
+        return element
     }
 
-    override fun supprimerParID(codeOrganisation: Int): Organisation? {
-        val uneOrganisation = db.queryForObject("select * from organisation where id = $codeOrganisation", OrganisationMapper())
+    override fun supprimerParID(id: Int): Organisation? {
+        val uneOrganisation = db.queryForObject("select * from organisation where id = $id", OrganisationMapper())
          db.update(
-            "delete from organisation where id = $codeOrganisation"
+            "delete from organisation where id = $id"
         )
         return uneOrganisation
     }
