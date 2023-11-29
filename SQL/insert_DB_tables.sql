28c8745de02bfe84773c695808ba6354a837702e
Alexandre Baudouin Vegas <alexrbvegas@gmail.com>
2023-11-28 22:00:07 -0500

    fixed sql error

--- SQL/insert_DB_tables.sql
+++ SQL/insert_DB_tables.sql
@@ -20,10 +20,10 @@ INSERT INTO `eventichsBD`.`Événement` (`nom`, `adresse`,`dateDebut`, `dateFin`
 INSERT INTO `eventichsBD`.`Événement` (`nom`, `adresse`, `dateDebut`, `dateFin`, `type`, `categorie_id`, `description`, `organisation_id`) VALUES ('Soiré3', '45 13e Avenue','2023-12-10', '2020-12-12', 'private', '1', 'Les détails de cette soirée ne sont connus que par le président du madagascar', '1');
 
 -- Insertion Membres_événement
-INSERT INTO `eventichsbd`.`Utilisateur_événement` (`idUtilisateur`, `idEvenement`) VALUES ('1', '1');
-INSERT INTO `eventichsbd`.`Utilisateur_événement` (`idUtilisateur`, `idEvenement`) VALUES ('1', '3');
-INSERT INTO `eventichsbd`.`Utilisateur_événement` (`idUtilisateur`, `idEvenement`) VALUES ('2', '2');
-INSERT INTO `eventichsbd`.`Utilisateur_événement` (`idUtilisateur`, `idEvenement`) VALUES ('2', '3');
+INSERT INTO `eventichsBD`.`Utilisateur_événement` (`idUtilisateur`, `idEvenement`) VALUES ('1', '1');
+INSERT INTO `eventichsBD`.`Utilisateur_événement` (`idUtilisateur`, `idEvenement`) VALUES ('1', '3');
+INSERT INTO `eventichsBD`.`Utilisateur_événement` (`idUtilisateur`, `idEvenement`) VALUES ('2', '2');
+INSERT INTO `eventichsBD`.`Utilisateur_événement` (`idUtilisateur`, `idEvenement`) VALUES ('2', '3');
 
 -- Insertion Invitation
 INSERT INTO `eventichsBD`.`Invitation_événement` (`idExpediteur`, `idDestinataire`, `idÉvénement`, `status`) VALUES ('1', '1', '1', 'envoyé');
@@ -31,5 +31,5 @@ INSERT INTO `eventichsBD`.`Invitation_organisation` (`idDestinataire`, `idOrgani
 INSERT INTO `eventichsBD`.`Invitation_organisation` (`idOrganisation`, `jeton`, `status`) VALUES ('1', '9EIUYTBB', 'généré');
 
 -- Insertion Organisations_membres
-INSERT INTO `eventichsbd`.`Organisations_membres` (`id_organisation`, `id_utilisateur`) VALUES ('1', '1');
+INSERT INTO `eventichsBD`.`Organisations_membres` (`id_organisation`, `id_utilisateur`) VALUES ('1', '1');
 
