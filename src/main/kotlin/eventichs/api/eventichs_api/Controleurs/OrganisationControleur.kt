af8b2ade1bf07d7052ee4225fb3a4b1eb8404791
WildBabaloo <47115136+WildBabaloo@users.noreply.github.com>
2023-12-12 23:22:51 -0500

    Tests obtenir er obtenirParID pour organisation

--- src/main/kotlin/eventichs/api/eventichs_api/Controleurs/OrganisationControleur.kt
+++ src/main/kotlin/eventichs/api/eventichs_api/Controleurs/OrganisationControleur.kt
@@ -2,16 +2,47 @@ package eventichs.api.eventichs_api.Controleurs
 
 import eventichs.api.eventichs_api.Modèle.Organisation
 import eventichs.api.eventichs_api.Services.OrganisationService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.*
 @RestController
 @RequestMapping("\${api.base-path:}")
+@Tag(
+    name = "Organisation",
+    description = "Points d'accès aux ressources liées aux organisations inscrits au service"
+)
 class OrganisationControleur(val service: OrganisationService) {
+
+    @Operation(
+        summary = "Obtenir la liste des organisations",
+        description = "Retourne la liste de toutes les organisations inscrites au service",
+        operationId = "obtenirOrganisations"
+    )
     @GetMapping("/organisations")
     fun obtenirOrganisations() = service.chercherTous()
 
+    @Operation(
+        summary = "Obtenir la liste des organisations par ID",
+        description = "Retourne la liste de l'organisation inscrit au service selon l'ID donné",
+        operationId = "obtenirOrgasationsParID",
+        responses = [
+            ApiResponse(responseCode = "200", description = "L'organisation à été trouvé"),
+            ApiResponse(responseCode = "404", description = "L'organisation avec l'id recherché n'existe pas dans le service")
+        ]
+    )
     @GetMapping("/organisations/{id}")
     fun obtenirOrganisationParID(@PathVariable id: Int) = service.chercherParID(id)
 
+    @Operation(
+        summary = "Créer une organisation",
+        description = "Retourne l'organisation créée",
+        operationId = "ajouterOrganisation",
+        responses = [
+            ApiResponse(responseCode = "201", description = "L'organisation à été ajoutée"),
+            ApiResponse(responseCode = "500", description = "L'organisation n'a pas été ajoutée au service")
+        ]
+    )
     @PostMapping("/organisations")
     fun ajouterOrganisation(@RequestBody organisation: Organisation) = service.ajouter(organisation)
 
--- src/test/kotlin/eventichs/api/eventichs_api/Controleurs/OrganisationControleurTest.kt
+++ src/test/kotlin/eventichs/api/eventichs_api/Controleurs/OrganisationControleurTest.kt
@@ -1,7 +1,10 @@
 package eventichs.api.eventichs_api.Controleurs
 
+import eventichs.api.eventichs_api.Exceptions.RessourceInexistanteException
 import eventichs.api.eventichs_api.Modèle.Organisation
 import eventichs.api.eventichs_api.Services.OrganisationService
+import junit.framework.TestCase.assertEquals
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
@@ -11,6 +14,7 @@ import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.http.MediaType
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 
@@ -25,8 +29,17 @@ lateinit var service: OrganisationService
     @Autowired
     private lateinit var mockMvc: MockMvc
 
+
+
+    // -----------------------------------------------------------------------------------------------------------------
+    //
+    // @GetMapping("/organisations")
+    //
+    // -----------------------------------------------------------------------------------------------------------------
+
+
     @Test
-    fun `Étant donné un administrateur qui effectue une requete GET pour afficher toute les organisations lorsqu on exécute la requête on obtient un fichier JSON qui reqgroupe les organisations et un code de retour 200 `(){
+    fun `1- Étant donné un administrateur qui effectue une requete GET pour afficher toute les organisations lorsqu on exécute la requête on obtient un fichier JSON qui reqgroupe les organisations et un code de retour 200 `(){
         var listorganisation: MutableList<Organisation> = mutableListOf(Organisation(1,1,"Illuminati",1,false))
         //listorganisation?.add(Organisation(1,1,"Illuminati",1,false))
 
@@ -43,15 +56,44 @@ lateinit var service: OrganisationService
 
     }
 
+    // -----------------------------------------------------------------------------------------------------------------
+    //
+    // @GetMapping("/organisations/{id}")
+    //
+    // -----------------------------------------------------------------------------------------------------------------
 
+    // DOES NOT WORK YET
     @Test
-    fun `Étant donné une organisation qui effectue une recherche pour un participant non-exisatant et de l ajouter on obtient un code de retour 404`(){
-        TODO("Méthode non-implémentée")
+    fun `2- Étant donné un admin qui effectue une recherche pour un organisation non-exisatant et de l ajouter on obtient un code de retour 404`(){
+        Mockito.`when`(service.chercherParID(10)).thenReturn(null)
+
+        mockMvc.perform(get("/organisations/10")
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isNotFound)
+            .andExpect{ résultat ->
+                assertTrue(résultat.resolvedException is RessourceInexistanteException)
+                Assertions.assertEquals(
+                    "L'organisation ayant l'id de 10 est non existant",
+                    résultat.resolvedException?.message
+                )
+
+            }
     }
 
     @Test
-    fun `Étant donné une organisation qui effectue une recherche pour un participant exisatant et de l ajouter on obtient un JSON qui contient le code du participant et un code de retour 200`(){
-        TODO("Méthode non-implémentée")
+    fun `Étant donné un admin qui effectue une recherche pour une organisation exisatant on obtient un JSON qui contient l'organisation et un code de retour 200`(){
+        var uneOrganisation = Organisation(1,1,"Illuminati",1,false)
+
+        Mockito.`when`(service.chercherParID(1)).thenReturn(uneOrganisation)
+
+        mockMvc.perform(get("/organisations/1"))
+            .andExpect(status().isOk)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.id").value(1))
+            .andExpect(jsonPath("$.idUtilisateur").value(1))
+            .andExpect(jsonPath("$.catégorie_id").value(1))
+            .andExpect(jsonPath("$.nomOrganisation").value("Illuminati"))
+            .andExpect(jsonPath("$.estPublic").value(false))
     }
 
     @Test
