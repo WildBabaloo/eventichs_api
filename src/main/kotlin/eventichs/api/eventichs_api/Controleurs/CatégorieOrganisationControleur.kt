577ed373aeca1c1b50879bfac05f9776eac58d56
Mahfu <mahfuzur.ali@hotmail.com>
2023-12-21 16:36:46 -0500

    Implémentation du Auth0 dans CatégorieOrganisationControleur (et modifications appropriées de CatégorieOrganisationService)

--- src/main/kotlin/eventichs/api/eventichs_api/Controleurs/CatégorieOrganisationControleur.kt
+++ src/main/kotlin/eventichs/api/eventichs_api/Controleurs/CatégorieOrganisationControleur.kt
@@ -1,10 +1,16 @@
 package eventichs.api.eventichs_api.Controleurs
 
+import eventichs.api.eventichs_api.Exceptions.PasConnectéException
 import eventichs.api.eventichs_api.Modèle.Catégorie
+import eventichs.api.eventichs_api.Services.CatégorieOrganisationService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder
+import java.security.Principal
 
 @RestController
 @RequestMapping("\${api.base-path:}")
@@ -12,20 +18,112 @@ import org.springframework.web.bind.annotation.*
     name = "CatégorieOrganisation",
     description = "Points d'accès aux ressources liées aux catégories d'organisations existantes sur le service."
 )
-class CatégorieOrganisationControleur {
-    @PostMapping("/organisations/categories_")
-    fun inscrire(@RequestBody catégorie: Catégorie):
-            ResponseEntity<Catégorie> = ResponseEntity(HttpStatus.NOT_IMPLEMENTED)
+class CatégorieOrganisationControleur(val service: CatégorieOrganisationService) {
 
+    @Operation(
+        summary = "Obtenir une catégorie spécifique par son ID.",
+        description = "Retourne la catégorie qui porte l'ID spécifié dans tous les catégories du service.",
+        operationId = "obtenirCategorieParID",
+        responses = [
+            ApiResponse(responseCode = "201", description = "Catégorie trouvé"),
+            ApiResponse(responseCode = "401", description = "L'utilisateur n'est pas connecté"),
+            ApiResponse(responseCode = "403", description = "L'utilisateur n'as pas le droit de consulter cette catégorie"),
+            ApiResponse(responseCode = "404", description = "La catégorie n'existe pas dans le service")
+        ]
+    )
+    @PostMapping("/organisations/categories")
+    fun obtenirCategories(principal: Principal?): List<Catégorie>{
+        if (principal == null) {
+            throw PasConnectéException("L'utilisateur n'est pas connecté.")
+        }
+        return service.chercherTous(principal.name)
+    }
+
+    @Operation(
+        summary = "Obtenir une catégorie spécifique par son ID.",
+        description = "Retourne la catégorie qui porte l'ID spécifié dans tous les catégories du service.",
+        operationId = "obtenirCategorieParID",
+        responses = [
+            ApiResponse(responseCode = "201", description = "Catégorie trouvé"),
+            ApiResponse(responseCode = "401", description = "L'utilisateur n'est pas connecté"),
+            ApiResponse(responseCode = "403", description = "L'utilisateur n'as pas le droit de consulter cette catégorie"),
+            ApiResponse(responseCode = "404", description = "La catégorie n'existe pas dans le service")
+        ]
+    )
     @GetMapping("/organisations/categories/{id}")
-    fun connexion(@PathVariable id: Int):
-            ResponseEntity<Catégorie> = ResponseEntity(HttpStatus.NOT_IMPLEMENTED)
+    fun obtenirCategorieParID(@PathVariable id: Int, principal: Principal?): Catégorie?{
+        if (principal == null) {
+            throw PasConnectéException("L'utilisateur n'est pas connecté.")
+        }
+        return service.chercherParID(id, principal.name)
+    }
 
-    @PutMapping("/organisations/categories/{id}")
-    fun modifier(@PathVariable id: Int, @RequestBody catégorie: Catégorie):
-            ResponseEntity<Catégorie> = ResponseEntity(HttpStatus.NOT_IMPLEMENTED)
+    @Operation(
+        summary = "Modifier une catégorie existante.",
+        description = "Modifie et retourne la catégorie spécifiée dans la base de données.",
+        operationId = "modifierCategorie",
+        responses = [
+            ApiResponse(responseCode = "200", description = "La catégorie a été modifié"),
+            ApiResponse(responseCode = "401", description = "L'utilisateur n'est pas connecté"),
+            ApiResponse(responseCode = "403", description = "L'utilisateur n'as pas le droit de consulter cette catégorie"),
+            ApiResponse(responseCode = "404", description = "La catégorie n'existe pas.")
+        ]
+    )
+    @GetMapping("/organisations/categories/{id}")
+    fun modifierCategorie(@PathVariable id: Int, @RequestBody catégorie: Catégorie, principal: Principal?): ResponseEntity<Catégorie> {
+        if (principal == null) {
+            throw PasConnectéException("L'utilisateur n'est pas connecté.")
+        }
+        val catégorieModifiée : Catégorie? = service.modifierCatégorie(catégorie, principal)
+        val uri = ServletUriComponentsBuilder
+            .fromCurrentRequest()
+            .path("/categories/{id}")
+            .buildAndExpand(catégorie.id)
+            .toUri()
+        return ResponseEntity.created(uri).body(catégorieModifiée)
+    }
 
+    @Operation(
+        summary = "Ajouter une nouvelle catégorie.",
+        description = "Créer et ajouter une nouvelle catégorie dans la base de données.",
+        operationId = "ajouterCategorie",
+        responses = [
+            ApiResponse(responseCode = "200", description = "La catégorie a été ajouté"),
+            ApiResponse(responseCode = "401", description = "L'utilisateur n'est pas connecté"),
+            ApiResponse(responseCode = "403", description = "L'utilisateur n'as pas le droit de consulter cette catégorie"),
+            ApiResponse(responseCode = "404", description = "La catégorie n'existe pas")
+        ]
+    )
+    @PutMapping("/organisations/categories")
+    fun ajouterCategorie(@RequestBody catégorie: Catégorie, principal: Principal?): ResponseEntity<Catégorie> {
+        if (principal == null) {
+            throw PasConnectéException("L'utilisateur n'est pas connecté.")
+        }
+        val nouvelleCatégorie: Catégorie? = service.ajouterCatégorie(catégorie, principal)
+        val uri = ServletUriComponentsBuilder
+            .fromCurrentRequest()
+            .path("/categories/{id}")
+            .buildAndExpand(catégorie.id)
+            .toUri()
+        return ResponseEntity.created(uri).body(nouvelleCatégorie)
+    }
+
+    @Operation(
+        summary = "Supprimer une catégorie spécifique par son ID.",
+        description = "Supprime et retourne la catégorie qui porte l'ID spécifié dans tous les catégories du service.",
+        operationId = "effacerCategorie",
+        responses = [
+            ApiResponse(responseCode = "200", description = "La catégorie a été supprimé"),
+            ApiResponse(responseCode = "401", description = "L'utilisateur n'est pas connecté"),
+            ApiResponse(responseCode = "403", description = "L'utilisateur n'as pas le droit de consulter cette catégorie"),
+            ApiResponse(responseCode = "404", description = "La catégorie n'existe pas")
+        ]
+    )
     @DeleteMapping("/organisations/categories/{id}")
-    fun effacer(@PathVariable id: Int):
-            ResponseEntity<Catégorie> = ResponseEntity(HttpStatus.NOT_IMPLEMENTED)
+    fun effacerCategorie(@PathVariable id: Int, principal: Principal?): Catégorie? {
+        if (principal == null) {
+            throw PasConnectéException("L'utilisateur n'est pas connecté.")
+        }
+        return service.supprimerParID(id)
+    }
 }
