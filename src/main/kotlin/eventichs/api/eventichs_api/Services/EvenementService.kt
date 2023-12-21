daef9c5701fb8d474226b57216b4961d48d21a7b
Alexandre Baudouin Vegas <abaudouinvegas@dept-info.crosemont.quebec>
2023-12-21 15:27:02 -0500

    Controlleur eveenement DOC + AUTH

--- src/main/kotlin/eventichs/api/eventichs_api/Services/EvenementService.kt
+++ src/main/kotlin/eventichs/api/eventichs_api/Services/EvenementService.kt
@@ -3,14 +3,15 @@ package eventichs.api.eventichs_api.Services
 import org.springframework.stereotype.Service
 import eventichs.api.eventichs_api.DAO.EvenementDAO
 import eventichs.api.eventichs_api.Modèle.Événement
+import java.security.Principal
 
 @Service
 class EvenementService(val dao : EvenementDAO) {
-    fun chercherTous(): List<Événement> = dao.chercherTous()
-    fun chercherParID(id: Int): Événement? = dao.chercherParID(id)
-    fun chercherParOrganisation(id: Int) : List<Événement> = dao.chercherParOrganisation(id)
+    fun chercherTous(name: String): List<Événement> = dao.chercherTous()
+    fun chercherParID(id: Int, name: String): Événement? = dao.chercherParID(id)
+    fun chercherParOrganisation(id: Int, name: Any) : List<Événement> = dao.chercherParOrganisation(id)
     fun chercherEvenementPublic() : List<Événement> = dao.chercherEvenementPublic()
     fun supprimerParID(id:Int) : Événement? = dao.supprimerParID(id)
-    fun modifierEvenement(id:Int, evenement : Événement ) : Événement? = dao.modifier(id,evenement)
-    fun ajouterEvenement(evenement : Événement ) : Événement? = dao.ajouter(evenement)
+    fun modifierEvenement(id: Int, evenement: Événement, principal: Principal?) : Événement? = dao.modifier(id,evenement)
+    fun ajouterEvenement(evenement: Événement, principal: Any?) : Événement? = dao.ajouter(evenement)
 }
