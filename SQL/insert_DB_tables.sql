2a093f409c8a5ca669fa63ff1d65488d22c492bc
WildBabaloo <47115136+WildBabaloo@users.noreply.github.com>
2023-12-21 15:56:21 -0500

    SQL modification insert

--- SQL/insert_DB_tables.sql
+++ SQL/insert_DB_tables.sql
@@ -16,8 +16,7 @@ INSERT INTO `eventichsBD`.`Catégorie_Organisation` (`nom`, `description`) VALUE
 
 -- Insertion Organisation
 INSERT INTO `eventichsBD`.`Organisation` (`nomOrganisation`, `codeUtilisateur`, `catégorie_id`, `estPublic`) VALUES ('Rosemont', 'auth0|656d2dbea19599c9209a4f01', '1', true);
-INSERT INTO `eventichsBD`.`Organisation` (`nomOrganisation`, `codeUtilisateur`, `catégorie_id`, `estPublic`) VALUES ('Les JoeBios', 'auth0|656d3cb6a19599c9209a6099', '1', true);
-INSERT INTO `eventichsBD`.`Organisation` (`nomOrganisation`, `codeUtilisateur`, `catégorie_id`, `estPublic`) VALUES ('FBI', 'auth0|656d3d344178aefc03429343', '1', false);
+INSERT INTO `eventichsBD`.`Organisation` (`nomOrganisation`, `codeUtilisateur`, `catégorie_id`, `estPublic`) VALUES ('FBI', 'auth0|656d3cb6a19599c9209a6099', '1', false);
 
 -- Insertion Événement
 INSERT INTO `eventichsBD`.`Événement` (`nom`, `adresse`,`dateDebut`, `dateFin`, `type`, `categorie_id`, `description`,  `organisation_id`) VALUES ('Soiré','134 avenue Parc', '2023-10-10', '2020-10-11', 'public', '1', 'soirée très cool', '1');
