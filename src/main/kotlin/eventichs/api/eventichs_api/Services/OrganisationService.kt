7f7a103390f7a7b4158ced5ba16c0634e6bd6dae
WildBabaloo <47115136+WildBabaloo@users.noreply.github.com>
2023-12-21 10:08:41 -0500

    Point d'accès organisation pour le service

--- src/main/kotlin/eventichs/api/eventichs_api/Services/OrganisationService.kt
+++ src/main/kotlin/eventichs/api/eventichs_api/Services/OrganisationService.kt
@@ -1,6 +1,7 @@
 package eventichs.api.eventichs_api.Services
 
 import eventichs.api.eventichs_api.DAO.OrganisationDAO
+import eventichs.api.eventichs_api.Exceptions.DroitAccèsInsuffisantException
 import eventichs.api.eventichs_api.Modèle.Organisation
 import org.springframework.stereotype.Service
 
@@ -8,9 +9,27 @@ import org.springframework.stereotype.Service
 class OrganisationService(val dao: OrganisationDAO) {
     fun chercherTous(): List<Organisation> = dao.chercherTous()
     fun chercherParID(id: Int): Organisation? = dao.chercherParID(id)
-    fun ajouter(element: Organisation): Organisation? = dao.ajouter(element)
-    fun supprimerParID(id: Int): Organisation? = dao.supprimerParID(id)
-    fun modifier(element: Organisation): Organisation? = dao.modifier(element)
+    fun ajouter(element: Organisation, codeUtilisateur: String): Organisation? {
+        if (!dao.validerUtilisateur(element.id, codeUtilisateur)) {
+            throw DroitAccèsInsuffisantException("L'utilisateur n'as pas le droit de consulter cette organisation")
+        }
+
+        return dao.ajouter(element)
+    }
+    fun supprimerParID(id: Int, codeUtilisateur: String): Organisation? {
+        if (!dao.validerUtilisateur(id, codeUtilisateur)) {
+            throw DroitAccèsInsuffisantException("L'utilisateur n'as pas le droit de consulter cette organisation")
+        }
+
+        return dao.supprimerParID(id)
+    }
+    fun modifier(element: Organisation, codeUtilisateur: String): Organisation? {
+        if (!dao.validerUtilisateur(element.id, codeUtilisateur)) {
+            throw DroitAccèsInsuffisantException("L'utilisateur n'as pas le droit de consulter cette organisation")
+        }
+
+        return dao.modifier(element)
+    }
     fun consulterOrganisationPubliques(): List<Organisation> = dao.consulterOrganisationPubliques()
     fun filtrerOrganisationParGouts(idCategorie: Int): List<Organisation> = dao.filtrerOrganisationParGouts(idCategorie)
 }
