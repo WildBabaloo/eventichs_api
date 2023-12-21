a7f8e41a33d2dc24ce2ae21e0344e7ef965e81d6
WildBabaloo <47115136+WildBabaloo@users.noreply.github.com>
2023-12-21 16:42:10 -0500

    Modification DAO organisation membres + service

--- src/main/kotlin/eventichs/api/eventichs_api/Services/OrganisationMembresService.kt
+++ src/main/kotlin/eventichs/api/eventichs_api/Services/OrganisationMembresService.kt
@@ -1,14 +1,27 @@
 package eventichs.api.eventichs_api.Services
 
 import eventichs.api.eventichs_api.DAO.OrganisationMembersDAO
+import eventichs.api.eventichs_api.Exceptions.DroitAccèsInsuffisantException
 import eventichs.api.eventichs_api.Modèle.OrganisationMembres
 import org.springframework.stereotype.Service
 
 @Service
 class OrganisationMembresService(val dao: OrganisationMembersDAO) {
     fun chercherTous(): List<OrganisationMembres> = dao.chercherTous()
-    fun ajouterParticipant(codeOrganisation: Int, codeUtilisateur: String) = dao.ajouterParticipant(codeOrganisation, codeUtilisateur)
-    fun enleverParticipant(codeOrganisation: Int, codeUtilisateur: String) = dao.enleverParticipant(codeOrganisation, codeUtilisateur)
+    fun ajouterParticipant(codeOrganisation: Int, codeUtilisateur: String) {
+        if (!dao.validerUtilisateur(codeOrganisation ,codeUtilisateur)){ throw DroitAccèsInsuffisantException("L'utilisateur n'as pas le droit de consulter cette organisation") }
+
+        dao.ajouterParticipant(codeOrganisation, codeUtilisateur)
+    }
+    fun enleverParticipant(codeOrganisation: Int, codeUtilisateur: String) {
+        if (!dao.validerUtilisateur(codeOrganisation, codeUtilisateur)){ throw DroitAccèsInsuffisantException("L'utilisateur n'as pas le droit de consulter cette organisation") }
+
+        dao.enleverParticipant(codeOrganisation, codeUtilisateur)
+    }
     fun chercherParParticipantID(codeUtilisateur: String): List<OrganisationMembres> = dao.chercherParUtilisateurID(codeUtilisateur)
-    fun chercherParOrganisationID(id: Int): List<OrganisationMembres> = dao.chercherParOrganisationID(id)
+    fun chercherParOrganisationID(codeOrganisation: Int, codeUtilisateur: String): List<OrganisationMembres> {
+        if (!dao.validerUtilisateur(codeOrganisation, codeUtilisateur)){ throw DroitAccèsInsuffisantException("L'utilisateur n'as pas le droit de consulter cette organisation") }
+
+        return dao.chercherParOrganisationID(codeOrganisation)
+    }
 }
